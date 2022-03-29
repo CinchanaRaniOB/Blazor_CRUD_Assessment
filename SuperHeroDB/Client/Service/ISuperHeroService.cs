aee300c327228bc13bda61100901369e16853e12
O B <cinchanarani.ob@dxc.com>
2022-03-29 21:10:38 +0530

    Blazor_CRUD_Project

---
+++ SuperHeroDB/Client/Service/ISuperHeroService.cs
@@ -0,0 +1,21 @@
+﻿using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Client.Service
+{
+    public interface ISuperHeroService
+    {
+        event Action Onchange;
+        List<Comic> Comics { get; set; }
+        List<SuperHero> Heroes { get; set; }
+        public Task<List<SuperHero>> GetSuperHeroes();
+        Task GetComics();
+        public Task<SuperHero> GetSuperHero(int id);
+        Task<List<SuperHero>> CreateSuperHero(SuperHero hero);
+        Task<List<SuperHero>> UpdateSuperHero(SuperHero hero, int id);
+        Task<List<SuperHero>> DeleteSuperHero(int id);
+    }
+}
